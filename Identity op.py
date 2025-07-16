4d7475930c4616c843604d42fd493971dc6299a0
Shaleenasyed <syedshaleena555@gmail.com>
2025-07-16 20:09:49 +0530

    Add files via upload

---
+++ Identity op.py
@@ -0,0 +1,4 @@
+num1=10
+num2=10
+print(num1 is num2)
+print(num1 is not num2)
---
+++ Membership op.py
@@ -0,0 +1,6 @@
+x="shaleena"
+print("s" in x)
+print("h" in x)
+print("a" not in x)
+print("l" not in x)
+print("u"in x)
